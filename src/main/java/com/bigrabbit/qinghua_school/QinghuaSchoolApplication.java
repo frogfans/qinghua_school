db0754f820bd89eca0ea00ebabdfe08c11b5610f
afu <email@example.com>
2019-06-03 20:19:51 +0800

    Initial commit

---
+++ src/main/java/com/bigrabbit/qinghua_school/QinghuaSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.bigrabbit.qinghua_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QinghuaSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QinghuaSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigrabbit/qinghua_school/QinghuaSchoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bigrabbit.qinghua_school;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QinghuaSchoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
