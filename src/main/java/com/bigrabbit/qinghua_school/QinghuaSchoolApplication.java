36dfc76bf8991c95f43f76a8a6d1b8873f242446
afu <email@example.com>
2019-06-06 09:15:15 +0800

    moban

--- src/main/java/com/bigrabbit/qinghua_school/QinghuaSchoolApplication.java
+++ src/main/java/com/bigrabbit/qinghua_school/QinghuaSchoolApplication.java
@@ -1,9 +1,13 @@
 package com.bigrabbit.qinghua_school;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.bigrabbit.qinghua_school.dao")
+@EnableSwagger2
 public class QinghuaSchoolApplication {
 
     public static void main(String[] args) {
