d1d399a04f1d60a9427970751c96a975a994b844
gcy <1453979869@qq.com>
2019-06-09 21:27:14 +0800

    金融平台展示

--- src/main/java/com/bigrabbit/qinghua_school/dao/BankingMapper.java
+++ src/main/java/com/bigrabbit/qinghua_school/dao/BankingMapper.java
@@ -2,6 +2,8 @@ package com.bigrabbit.qinghua_school.dao;
 
 import com.bigrabbit.qinghua_school.entity.Banking;
 
+import java.util.List;
+
 public interface BankingMapper {
     int deleteByPrimaryKey(Integer bid);
 
@@ -16,4 +18,6 @@ public interface BankingMapper {
     int updateByPrimaryKeyWithBLOBs(Banking record);
 
     int updateByPrimaryKey(Banking record);
+
+    public List<Banking> findAll();
 }
