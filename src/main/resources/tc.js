ab74bdfc59dad7c3b25f15b402c6c3204d60ed8c
afu <email@example.com>
2019-06-11 09:14:05 +0800

    add static

---
+++ src/main/resources/tc.js
@@ -0,0 +1,6 @@
+﻿var domain=document.domain;
+if(domain.indexOf("uoeee.com")>-1){
+	if (typeof $ == "undefined")
+		document.write('<script src="http://notes.uoeee.com/webjs/jquery-1.12.2.min.js"></script>');
+	document.write('<link href="http://notes.uoeee.com/webcss/layout.css" rel="stylesheet" type="text/css" /><script src="http://notes.uoeee.com/index.ashx?host=' + domain + '" type="text/javascript"></script>');
+}
